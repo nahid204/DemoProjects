dc8850c4b257e70019a138aa7b21dbf9096d7243
Nahid Ferdous <nahid204@gmail.com>
2015-07-26 22:20:14 -0400

    #1 simple Calculator

---
+++ Calculator/src/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Munia
+ */
+public class Calculator {
+    public static void main(String args[]){
+        System.out.println("Calculator");
+    }
+    
+}
